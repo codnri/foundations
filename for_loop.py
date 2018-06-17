32830d6a958582ab344d02d9eda3f37e6ce0917c
izo <izo@kodak.com>
2018-06-17 00:50:17 -0400

    finish gun_deaths

--- for_loop.py
+++ for_loop.py
@@ -12,12 +12,23 @@ print("Hi {0}, Good {1}".format("Tom","Evening"))
 print("Hi {2}, Good {1}. What would like to have for {0}".format("Dinner","Evening","Tom"))
 
 
-print("***dictionary***")
-# dictTest = {"oneKey":1, "twoKey":2, "threeKey":3}
-# print("oneKey:"+str(dictTest["oneKey"]))
-# for val in dictTest.keys():
-#   print(val+":::"+str(dictTest[val]))
+# print("***dictionary***")
+# # dictTest = {"oneKey":1, "twoKey":2, "threeKey":3}
+# # print("oneKey:"+str(dictTest["oneKey"]))
+# # for val in dictTest.keys():
+# #   print(val+":::"+str(dictTest[val]))
 
-mydict = {"apple":1, "orange":2, "banana":3}
-mydict["peach"] = 4
-print(mydict)
+# mydict = {"apple":1, "orange":2, "banana":3}
+# mydict["peach"] = 4
+# print(mydict)
+
+# lst = []
+# print("list comprehension")
+# line= '"12",2012,"01","Suicide",0,"M",21,"Native American/Native Alaskan",100,"Home",2'
+# lst = [ int(element) if element.isdigit() else element.strip('"') for element in line.strip().split(',') ]
+# print(lst)
+
+print("list comprehension")
+fruits = ['apple', 'banana', 'melon', 'apple', 'apple', 'apple']
+lst= set(f for f in fruits)
+print(lst)
